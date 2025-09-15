d4fd592699257772baa4bdf2511c3d25d6373421
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-09-15 06:12:10 +0000

    Js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 let canvas = document.querySelector("canvas")
 let pen = canvas.getContext("2d")
 
-pen.fillStyle="rgba(255, 5, 5, 1)"
-pen.fillRect(10, 10, 40, 40)
-pen.clearRect(10, 10, 40, 40)
+
+
+let snakeCells = [[2,2]];
+let cell = 50;
+
+function draw () {
+    for(let i of snakeCells){
+        pen.fillStyle="rgba(255, 5, 5, 1)";
+        pen.fillRect(i[0], i[1], cell, cell);
+    };
+}
+draw();
+
+function update () {
+
+}
